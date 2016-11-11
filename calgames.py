f15f7161f3c78c8f020f9a5fa5d00405b2f35948
Robbie Selwyn <robbie@selwynsoftware.com>
2016-11-10 22:21:28 -0800

    Added calgames match list generator

--- calgames.py
+++ calgames.py
@@ -1,13 +1,13 @@
 from TBAconnection import *
 from subprocess import call
 
-matches = get_matches_with_teams("2016cc")
+matches = get_matches_with_teams("2016cacc")
 
 f = open("out.csv", "w")
 
 for match in matches:
 	data = []
-	
+
 	if match.get_key_as_displayable().isdigit():
 		data.append([match.get_key_as_displayable(),match.get_blue_alliance().as_string(),match.get_red_alliance().as_string()])
 
@@ -26,8 +26,8 @@ f.close()
 with open("realout.csv", "w") as f:
 	for line in data:
 		for team in line.split(",")[1].split(" "):
-			f.write(team + "," + line.split(",")[0] + ", BLUE \n" )
+                    f.write(team[3:] + "," + line.split(",")[0] + ", BLUE \n" )
 		for team in line.split(",")[2].split(" "):
-			f.write(team.replace("\n","") + "," + line.split(",")[0] + ", RED \n")
+                    f.write(team.replace("\n","")[3:] + "," + line.split(",")[0] + ", RED \n")
 
 call(["rm", "out.csv"])
