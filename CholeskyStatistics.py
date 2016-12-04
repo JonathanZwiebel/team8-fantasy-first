db51f9c3fffe55d34ef2bf7ad6082b7f5979cb0b
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-12-04 01:52:26 -0800

    Added new round of statistics to Cholesky calculator

--- CholeskyStatistics.py
+++ CholeskyStatistics.py
@@ -2,7 +2,9 @@
 # multiple statistics can be calculated in quick succession. This procedure takes 0.5n^2 + (k + 0.5)n row operations where n is the number of teams at the event and k is the number of statistics to calculate
 #
 # Currently implemented statistics:
-#  - OPR_total_points
+#  - OPR Total Points
+#  - High Goals
+#  - 
 #
 # Author: Jonathan Zwiebel
 # Version: 28 November 2016
@@ -23,7 +25,7 @@ for team in teams:
 	indexed_teams.append(str(team.get_key()))
 
 # Fills coefficient and resultant vector with zeroes
-statistics = ["Total Points", "High Goals", "Auto Points", "Ranking Points", "Breach Achieved", "Defense Crossings"]
+statistics = ["Total Points", "High Goals", "Auto Points", "Ranking Points", "Breach Achieved", "Defense Crossings", "Tower Damage", "Scale Points", "Winning Margin"]
 A = sp.zeros((len(teams),len(teams)),dtype=int)
 B = sp.zeros((len(teams),len(statistics)),dtype=int)
 
@@ -52,6 +54,12 @@ for match in matches:
 			B[indexed_teams.index(red_teams[k])][4] += match.get_red_breach()
 			B[indexed_teams.index(blue_teams[k])][5] += match.get_blue_crossings()
 			B[indexed_teams.index(red_teams[k])][5] += match.get_red_crossings()
+			B[indexed_teams.index(blue_teams[k])][6] += match.get_blue_tower_damage()
+			B[indexed_teams.index(red_teams[k])][6] += match.get_red_tower_damage()
+			B[indexed_teams.index(blue_teams[k])][7] += match.get_blue_scale_points()
+			B[indexed_teams.index(red_teams[k])][7] += match.get_red_scale_points()
+			B[indexed_teams.index(blue_teams[k])][7] += match.get_blue_winning_margin()
+			B[indexed_teams.index(red_teams[k])][7] += match.get_red_winning_margin()
 
 
 # Removes teams that did not play to keep matrix Hermitian
@@ -70,6 +78,8 @@ while index < len(A):
 # Cholesky decomposition
 # cond = sp.linalg.expm_cond(A)
 L = sp.linalg.cholesky(A, lower=True, overwrite_a=True, check_finite=False)
+sp.savetxt("Pairings.csv", sp.asarray(L), delimiter=",")
+
 
 # Forward substitution
 Z = sp.linalg.solve_triangular(L, B, lower=True, trans=0, unit_diagonal=False, overwrite_b=True, check_finite=False)
@@ -82,15 +92,16 @@ X = sp.linalg.solve_triangular(Lt, Z, lower=False, trans=0, unit_diagonal=False,
 
 # Prints results
 sp.set_printoptions(threshold=sp.inf,suppress=True)
-result = sp.empty(shape=(len(X),7))
+result = sp.empty(shape=(len(X),len(statistics) + 1))
 for i in range(len(X)):
 	result[i][0] = int(indexed_teams[i][3:])
 	for j in range(1, len(statistics) + 1):
 		result[i][j] = float(X[i][j-1])
-result = result[sp.array(result[:,1].argsort(axis=0).tolist()).ravel()]
+sortby = "Winning Maring"
+result = result[sp.array(result[:,statistics.index(sortby) + 1].argsort(axis=0).tolist()).ravel()]
 result = sp.flipud(result)
 
-print "Teams by OPR of Points"
+print "Teams by CCWMs"
 print result
 
 sp.savetxt("Results.csv", sp.asarray(result), delimiter=",")
