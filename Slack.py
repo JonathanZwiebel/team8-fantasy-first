f0347eadb49ed2b3f6d33e97f2f0ed8d18b4ab03
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-12-31 15:42:37 -0800

    Updated slack to include attachement in payload

--- Slack.py
+++ Slack.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-def send_message(data, channel="#fantasy-first", icon=":robot_face:", name="Fantasy FIRST Bot"):
+def send_message(data, attach="", channel="#fantasy-first", icon=":robot_face:", name="Fantasy FIRST Bot"):
 	f = open("data/slackurl.txt")
-	payload = {"channel": channel, "username": name,"text": data, "icon_emoji": icon}
+	payload = {"channel": channel, "username": name,"text": data, "icon_emoji": icon, "attachments": attach}
 	results = requests.post(f.readline(), json.dumps(payload), headers={'content-type': 'application/json'})
 
 	f.close()
