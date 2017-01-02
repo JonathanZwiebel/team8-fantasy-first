9d3fe97fd3df3a42d38bcf74cb6fa4c882d169bf
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2017-01-01 19:59:44 -0800

    Added timeout to Slack requset

--- Slack.py
+++ Slack.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def send_message(data, attach="", channel="#fantasy-first", icon=":robot_face:", name="Fantasy FIRST Bot"):
+def send_message(data, attach="", channel="#fantasy-first-bot", icon=":robot_face:", name="Fantasy FIRST Bot"):
 	f = open("data/slackurl.txt")
 	payload = {"channel": channel, "username": name,"text": data, "icon_emoji": icon, "attachments": attach}
-	results = requests.post(f.readline(), json.dumps(payload), headers={'content-type': 'application/json'})
+	results = requests.post(f.readline(), json.dumps(payload), headers={'content-type': 'application/json'}, timeout=1)
 
 	f.close()
 	return results.text
