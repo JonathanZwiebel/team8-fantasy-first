f6fdc7f5f487344fb221324a542f20b9985dee9e
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2017-03-23 20:29:51 -0700

    Extended timeout time on Slack call

--- Slack.py
+++ Slack.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def send_message(data, attach="", channel="#test", icon=":deankamen:", name="Fantasy FIRST Bot"):
+def send_message(data, attach="", channel="#fantasy-first", icon=":deankamen:", name="Fantasy FIRST Bot"):
 	f = open("data/slackurl.txt")
 	payload = {"channel": channel, "username": name,"text": data, "icon_emoji": icon, "attachments": attach}
-	results = requests.post(f.readline(), json.dumps(payload), headers={'content-type': 'application/json'}, timeout=1)
+	results = requests.post(f.readline(), json.dumps(payload), headers={'content-type': 'application/json'}, timeout=10)
 
 	f.close()
 	return results.text
